c260d03e2d6a654ed12fc608c58cb0b09a0a56cd
dimitraz <dimitrazuccarelli@gmail.com>
2018-10-11 18:07:50 +0100

    Empty hillfort list activity

---
+++ app/src/main/java/org/wit/hillfort/activities/HillfortListActivity.kt
@@ -0,0 +1,16 @@
+package org.wit.hillfort.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.wit.hillfort.R
+import org.wit.hillfort.main.MainApp
+
+class HillfortListActivity : AppCompatActivity() {
+  lateinit var app: MainApp
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_hillfort_list)
+    app = application as MainApp
+  }
+}
