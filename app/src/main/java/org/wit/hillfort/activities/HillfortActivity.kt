9d73d6d5d612f4c75822ddee772079c60214fa1a
dimitraz <dimitrazuccarelli@gmail.com>
2018-10-11 18:02:02 +0100

    Add main app class

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -5,14 +5,17 @@ import android.os.Bundle
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.wit.hillfort.R
+import org.wit.hillfort.main.MainApp
 import org.wit.hillfort.models.HillfortModel
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
+  var app : MainApp? = null
   val hillfort = HillfortModel("Dun Aengus", "Prehistoric hillfort in the Aran Islands, County Galway")
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_hillfort)
+    app = application as MainApp
 
     info("Hillfort: $hillfort")
   }
