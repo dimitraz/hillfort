69eccce8c0fb520824363e94c847efd6bab1c50f
dimitraz <dimitrazuccarelli@gmail.com>
2018-10-19 12:23:10 +0100

    Splash screen activity

---
+++ app/src/main/java/org/wit/hillfort/activities/SplashActivity.kt
@@ -0,0 +1,37 @@
+package org.wit.hillfort.activities
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.intentFor
+import org.wit.hillfort.R
+
+class SplashActivity : AppCompatActivity() {
+  private lateinit var delayHandler: Handler
+  private val SPLASH_DELAY: Long = 1000 //3 seconds
+
+  internal val mRunnable: Runnable = Runnable {
+    if (!isFinishing) {
+      startActivity(intentFor<HillfortListActivity>())
+      finish()
+    }
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_splash)
+
+    // Initialize the Handler
+    delayHandler = Handler()
+
+    // Navigate with delay
+    delayHandler.postDelayed(mRunnable, SPLASH_DELAY)
+  }
+
+  public override fun onDestroy() {
+    if (delayHandler != null) {
+      delayHandler.removeCallbacks(mRunnable)
+    }
+    super.onDestroy()
+  }
+}
